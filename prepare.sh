d2e693103f5bb6c8fb2672cb9bebe8edcb12f900
Phillip Hall <talvor@me.com>
2022-08-10 20:36:49 +1000

    Initial

---
+++ prepare.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Enable Fastest Mirror 
+echo "### Enable fastest mirror"
+sudo sed -zi '/fastestmirror=true/!s/$/\nfastestmirror=true\n/' /etc/dnf/dnf.conf
+sudo sed -zi '/deltarpm=true/!s/$/deltarpm=true\n/' /etc/dnf/dnf.conf
+sudo sed -zi '/max_parallel_downloads=10/!s/$/max_parallel_downloads=10\n/' /etc/dnf/dnf.conf
+
+# Run system update
+echo "### Update system"
+sudo dnf update
+
+# Enable RPM Fusion
+echo "### Enable RPM Fusion"
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Reduce Battery Usage
+echo "### Reduce battery usage"
+sudo dnf install tlp tlp-rdw
+sudo systemctl enable tlp
+
+# Enable flathub repo
+echo "### Enable Flathub"
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Install Ansible
+echo "### Install ansible"
+sudo dnf install ansible
+
+# Install Refind boot manager
+echo "### Install refind boot manager"
+sudo dnf install "https://ixpeering.dl.sourceforge.net/project/refind/0.13.3.1/refind-0.13.3.1-1.x86_64.rpm"
+sudo refind-install
+curl -sL https://git.io/refind-theme-nord | bash
+
